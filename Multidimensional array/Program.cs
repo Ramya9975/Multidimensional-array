abb39acf6b5c50c5d2c994c73dd652b8ae144867
Ramyamani9975 <ramyamanivannan99@gmail.com>
2023-02-06 12:01:51 +0530

    Add project files.

---
+++ Multidimensional array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multidimensional_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int rowsum;
+            int[,] multiArray = new int[2, 5]
+            {
+                {2,2,2,2,2},
+                 {3,3,3,3,3}
+            };
+            for(int row = 0; row<2; row++)
+            {
+                rowsum = 0;
+                for(int col = 0; col < 5; col++) 
+                {
+                    Console.Write("{0}\t", multiArray[row, col]);
+                rowsum = rowsum+ multiArray[row, col];
+                }
+                sum = sum + rowsum;
+                Console.Write(" = {0}", rowsum);
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("The sum of the array is:{0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
