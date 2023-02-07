fdb945c60bb0d31e32725cb77a8a2eeeeab328e2
Ramya9975 <123952058+Ramya9975@users.noreply.github.com>
2023-02-07 16:48:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labexercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] employee = new string[5];
+            employee[0] = "Ram";
+            employee[1] = "Rekha";
+            employee[2] = "Ram";
+            employee[3] = "sri";
+            employee[4] = "radhu";
+
+            Console.WriteLine(employee[0]);  
+            Console.WriteLine(employee[1]);
+            Console.WriteLine(employee[2]);
+            Console.WriteLine(employee[3]);
+            Console.WriteLine(employee[4]);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
